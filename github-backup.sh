ab62fb416bd52c66f6e0385deb881ef2c43fed6a
Federico Mestrone <federico@moodsdesign.com>
2017-02-22 17:20:49 +0000

    Improved functionality and usability of script

--- github-backup.sh
+++ github-backup.sh
@@ -1,23 +1,80 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Simple shell script to backup all GitHub repos
-# Usage: github-backup.sh <username>
+# Usage: github-backup.sh -u <repo-user> | -o <repo-org> [--debug] -l <login-user>
 # @author Petr Trofimov <petrofimov@yandex.ru>
+# @author Federico Mestrone <fmestrone@gmail.com>
 #
 
-set -ex
+function echo_usage
+{
+    echo "Usage: github-backup.sh -u <repo-user>|-o <repo-org> -l <login-user> [--debug]"
+}
 
-USER="$1"
-API_URL="https://api.github.com/users/${USER}/repos?type=owner"
-DATE=$(date +"%Y%m%d")
-TEMP_DIR="github_${USER}_${DATE}"
-BACKUP_FILE="${TEMP_DIR}.tgz"
+if [ "$#" -eq 0 ]; then
+  echo "No option specified"
+  echo_usage
+  exit 1
+fi
 
-mkdir "$TEMP_DIR" && cd "$TEMP_DIR"
-curl -s "$API_URL" | grep -Eo '"git_url": "[^"]+"' | awk '{print $2}' | xargs -n 1 git clone
-cd -
-tar zcf "$BACKUP_FILE" "$TEMP_DIR"
-rm -rf "$TEMP_DIR"
+while [[ $# -gt 1 ]]
+do
+case $1 in
+    -o|--org)
+    GIT_TARGET="orgs/$2"
+    shift
+    ;;
+    -u|--user)
+    GIT_TARGET="users/$2"
+    shift
+    ;;
+    -l|--login-user)
+    GIT_USER="$2"
+    shift
+    ;;
+    --debug)
+    set -x
+    ;;
+    *)
+    echo "Unknown option $1"
+    echo_usage
+    exit 1
+    ;;
+esac
+shift
+done
 
+if [ "x$GIT_TARGET" = "x" ]; then
+  echo "No repo username or organization specified"
+  echo_usage
+  exit 1
+fi
 
+if [ "x$GIT_USER" = "x" ]; then
+  echo "No login username specified"
+  echo_usage
+  exit 1
+fi
+
+set -e
+
+GIT_API_URL="https://api.github.com/${GIT_TARGET}/repos?type=all"
+GIT_DATE=$(date +"%Y%m%d")
+GIT_TEMP_DIR="$(mktemp -q -d -t "github_${GIT_USER}_${GIT_DATE}")"
+GIT_BACKUP_FILE="github_${GIT_USER}_${GIT_DATE}.tgz"
+
+echo "Ready to back up all repos from ${GIT_API_URL}"
+echo "Temp target folder: $GIT_TEMP_DIR"
+echo "Final target file: $GIT_BACKUP_FILE"
+
+pushd "$GIT_TEMP_DIR"
+
+curl -u "$GIT_USER" -s "$GIT_API_URL" | grep -Eo '"clone_url": "[^"]+"' | awk '{print $2}' | xargs -n 1 git clone
+
+popd
+
+tar zcf "$GIT_BACKUP_FILE" "$GIT_TEMP_DIR"
+rm -Rf "$GIT_TEMP_DIR"
+
+exit 0
